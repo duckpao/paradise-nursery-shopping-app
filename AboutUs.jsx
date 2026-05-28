2b9b50d7347df13af4b280789ab9508194b02b2a
duckpao <156822180+duckpao@users.noreply.github.com>
2026-05-28 18:21:08 +0700

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Welcome to Paradise Nursery, where we bring life and green serenity straight into your living space. 
+        Our mission is to provide high-quality, sustainably grown houseplants that elevate your home decor 
+        and boost your well-being. From air-purifying ferns to low-maintenance succulents, we have something 
+        for every plant lover.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
